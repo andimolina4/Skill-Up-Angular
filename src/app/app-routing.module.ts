1fcd4e9c2d10ace402f5bf97b583536a1a8697f0
Sergio Peralta <seperalta88@gmail.com>
2022-11-03 00:02:42 -0300

    first routes done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [  
+const routes: Routes = [
+  {
+    path: 'auth/login',
+    loadChildren: () =>
+      import('./pages/auth-login/auth-login.module').then(
+        (m) => m.AuthLoginModule
+      ),
+  },
+  {
+    path: 'auth/registro',
+    loadChildren: () =>
+      import('./pages/auth-registro/auth-registro.module').then(
+        (m) => m.AuthRegistroModule
+      ),
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./pages/page-not-found/page-not-found.module').then(
+        (m) => m.PageNotFoundModule
+      ),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
