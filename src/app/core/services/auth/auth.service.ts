4b325338d9b8cb84d512f0c296c1e13268eeb345
alesenestrari <alesenestrari@outlook.com>
2022-11-07 11:47:50 -0300

    Login added working good

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,18 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UserRequest, UserResponse } from '@app/interfaces/user.interface';
+import { UserLoginRequest } from '@app/pages/auth-login/login/login.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  url = 'http://wallet-main.eba-ccwdurgr.us-east-1.elasticbeanstalk.com/users'
+  url = 'http://wallet-main.eba-ccwdurgr.us-east-1.elasticbeanstalk.com/users';
+  urlAuth = 'http://wallet-main.eba-ccwdurgr.us-east-1.elasticbeanstalk.com/auth';
 
   constructor(private http: HttpClient) { }
 
   signUp(user: UserRequest) {
     console.log(user)
     return this.http.post(this.url, user);
+  }
+
+  login(user: UserLoginRequest) {
+    return this.http.post<{'accessToken': string}>(this.urlAuth + '/login', user);
   }
 }
--- src/app/pages/auth-login/login/login.component.ts
+++ src/app/pages/auth-login/login/login.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '@app/core/services/auth/auth.service';
+
+export interface UserLoginRequest {
+  email: string,
+  password: string
+}
 
 @Component({
   selector: 'app-login',
@@ -7,9 +15,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user: UserLoginRequest = {
+    email: '',
+    password: ''
+  }
+
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onLogin() {
+    this.authService.login(this.user).subscribe({
+      next: res => {
+        localStorage.setItem('accessToken', res.accessToken);
+        this.router.navigate(['home/dashboard']);
+      },
+      error: err => {
+        console.log(err);
+      }
+    })
   }
 
 }
