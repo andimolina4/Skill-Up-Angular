4e19d231ae38fa1febfdc2ecfe49311aac3bb0fb
ZelayaGonzalo <zelayagonzalo33@gmail.com>
2022-11-07 17:57:45 -0300

    Added ng2-chart, Made graphic with dummy data,created graphic component

--- src/app/pages/home/home-page/home.component.ts
+++ src/app/pages/home/home-page/home.component.ts
@@ -1,9 +1,5 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '@app/core/services/auth/auth.service';
-import { Observable, map, shareReplay } from 'rxjs';
-import Swal from 'sweetalert2';
+import { faScaleBalanced, faPiggyBank, faTicket } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -11,84 +7,13 @@ import Swal from 'sweetalert2';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  panelOpenState = false;
-  isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe(Breakpoints.Handset)
-    .pipe(
-      map((result) => result.matches),
-      shareReplay()
-    );
-
-  menuItems = [
-    {
-      name: 'Dashboard',
-      icon: 'dashboard',
-      link: 'dashboard',
-    },
-    {
-      name: 'Saldo',
-      icon: 'savings',
-      link: 'saldo',
-    },
-    {
-      name: 'Gastos',
-      icon: 'receipt_long',
-      link: 'gastos',
-    },
-    {
-      name: 'Balance',
-      icon: 'balance',
-      link: 'balance',
-    },
-    {
-      name: 'Movimientos',
-      icon: 'swap_horiz',
-      link: 'movements',
-    },
-    {
-      name: 'Enviar Dinero',
-      icon: 'send',
-      link: 'send-founds',
-    },
-    {
-      name: 'Exchange',
-      icon: 'currency_exchange',
-      link: 'exchange',
-    },
-    {
-      name: 'Inversiones',
-      icon: 'trending_up',
-      link: 'investment',
-    },
-    {
-      name: 'Mi Cuenta',
-      icon: 'account_circle',
-      link: 'my-account',
-    },
-  ];
+  scaleIcon = faScaleBalanced
+  piggy = faPiggyBank
+  ticket = faTicket
 
   constructor(
-    private breakpointObserver: BreakpointObserver,
-    private router: Router,
-    private authService: AuthService
   ) {}
 
   ngOnInit(): void {}
 
-  logout() {
-    Swal.fire({
-      title: '¿Estás seguro?',
-      text: 'Estás a punto de cerrar sesión',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Sí, cerrar sesión',
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire('Sesión cerrada', '', 'success');
-        this.router.navigate(['/auth/login']);
-      }
-    });
-  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,11 +9,14 @@ import { MaterialModule } from '@app/material/material.module';
 import { FooterComponent } from './components/footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { GraphicsComponent } from './components/graphics/graphics.component';
+import { NgChartsModule } from 'ng2-charts';
+import { MovementsComponent } from './components/movements/movements.component';
 
 
 @NgModule({
-  declarations: [LoaderComponent, HeaderComponent, FooterComponent, SidebarComponent],
-  imports: [CommonModule, RouterModule, MaterialModule,FontAwesomeModule],
-  exports: [LoaderComponent, HeaderComponent, FooterComponent,SidebarComponent],
+  declarations: [LoaderComponent, HeaderComponent, FooterComponent, SidebarComponent, GraphicsComponent, MovementsComponent],
+  imports: [CommonModule, RouterModule, MaterialModule,FontAwesomeModule,NgChartsModule],
+  exports: [LoaderComponent, HeaderComponent, FooterComponent,SidebarComponent,GraphicsComponent],
 })
 export class SharedModule {}
