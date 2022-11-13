e47fe0113913d07997409a89db4aaaffbb9326e2
andimolina4 <andimolina629@gmail.com>
2022-11-13 18:11:30 -0300

    divisas

--- src/app/pages/home/components/exchange/exchange.component.ts
+++ src/app/pages/home/components/exchange/exchange.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { casa, ExchangeResponse } from '@app/core/models/exchange.model';
+import { ExchangeConvertService } from '@app/core/services/exchange/exchange.convert.service';
 
 @Component({
   selector: 'app-exchange',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exchange.component.scss']
 })
 export class ExchangeComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  exchangeResponse: ExchangeResponse = new ExchangeResponse (new casa())
+
+  error: string = ""
+
+  constructor(private exchangeService: ExchangeConvertService, private readonly formBuilder: FormBuilder) {
+    this.form = formBuilder.group({
+      pesos: ['', [Validators.required]],
+      dolares: ['']
+    })
+   }
+
 
   ngOnInit(): void {
+    this.exchangeService.getDolar().subscribe( (resp) => {
+      this.exchangeResponse = resp[0];
+      
+    },
+    (err) => {
+      this.error = err.message;
+    })
+
   }
 
+
+  onSubmit(){
+    this.form.patchValue({
+      dolares: this.exchangeService.convert(Number(this.form.get('pesos')?.value), true).toFixed(2)
+    });
+
+    //si pongo false, hace lo contrario
+  }
+
+  
+  
+
 }
+ 
